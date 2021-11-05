a28441e30fae81a030f80e09d11c1475bda487ad
Giancarlo <gian.fernandez@tecsup.edu.pe>
2021-11-05 11:21:03 -0500

    nuevos cambios

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvBroadCast;
+    private IntentFilter chargingIntentFilter;
+    private CharginBroadCastReceiver chargingBroadCastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        tvBroadCast = (TextView) findViewById(R.id.tvBroadCast);
+        chargingIntentFilter = new IntentFilter();
+        chargingIntentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        chargingIntentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        chargingBroadCastReceiver = new CharginBroadCastReceiver();
+
+    }
+    public class CharginBroadCastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent){
+            String action = intent.getAction();
+            boolean isChagin = (action.equals(Intent.ACTION_POWER_CONNECTED));
+            showCharging(isChagin);
+        }
+    }
+
+    private void showCharging(boolean isCharging){
+        if(isCharging){
+            tvBroadCast.setText("Conectado");
+            tvBroadCast.setTextColor(Color.parseColor("#0000FF"));
+        }
+        else{
+            tvBroadCast.setText("Desconectado");
+            tvBroadCast.setTextColor(Color.parseColor("#FF0000"));
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        registerReceiver(chargingBroadCastReceiver,chargingIntentFilter);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(chargingBroadCastReceiver);
     }
 }
